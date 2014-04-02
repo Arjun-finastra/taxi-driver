9466ab593276e1fba22ff3d4bcb3cdacef7279bc
Md. Zahidul Islam <zahidulislam2008@gmail.com>
2014-04-02 17:49:53 +0600

    beep updated

--- js/app.reg.device.js
+++ js/app.reg.device.js
@@ -59,6 +59,8 @@
 						//$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
 						
 						if(e.payload.message == 'New Job'){
+							navigator.notification.beep(3);
+							navigator.notification.vibrate(2500);
 							$('#popupBasic').popup('open');
 						}
                     break;
