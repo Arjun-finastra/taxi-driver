c1cca2983be884d483267a937a1ddb3a36c7fd96
positiveZahid <zahidulislam2008@gmail.com>
2014-04-30 15:06:30 +0600

    set time out

--- js/push.js
+++ js/push.js
@@ -86,9 +86,10 @@
 								}
 								navigator.notification.vibrate(1000);
 								var timer = setTimeout(function() {
+										alert('foreground');
 										params = { callback : 'callbackSendMail', controller : 'Users', action : 'sendMail', data : [{ stringText : jobRejected }] };
 										getAjaxData(params, 'callbackSendMail');
-								}, 15000);
+								}, 1000);
 								//$('#popupBasic').popup('open');
 								//window.location.reload();
 								//$('#popupBasic').popup('open');
@@ -105,9 +106,10 @@
 									//playAudio('/android_asset/www/chacha.wav');
 									navigator.notification.vibrate(1000);
 									var timer = setTimeout(function() {
+										alert('background');
 										params = { callback : 'callbackSendMail', controller : 'Users', action : 'sendMail', data : [{ stringText : jobRejected }] };
 										getAjaxData(params, 'callbackSendMail');
-								}, 15000);
+								}, 1000);
 									//window.location.reload();
 									//$('#popupBasic').popup('open');
 									
